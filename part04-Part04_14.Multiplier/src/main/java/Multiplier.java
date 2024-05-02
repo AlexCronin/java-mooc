67c895393c5ea4861ca859e75a27460148861e22
Alex Cronin <acalexliverpool@gmail.com>
2024-05-02 22:51:13 +0100

    Completed Part 4 Exercises 10 to 15

---
+++ part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acale
+ */
+public class Multiplier {
+    
+    private int number;
+    
+    public Multiplier(int multiplier) {
+        this.number = multiplier;
+    }
+    
+    public int multiply(int number) {
+        return this.number * number;
+    }
+    
+}
--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,38 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+/*        
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+*/      
+        Statistics statistics = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        Statistics statisticsOdd = new Statistics();
+        System.out.println("Enter numbers:");
+        
+        while(true) {
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if(num == -1) {
+                break;
+            }       
+            statistics.addNumber(num);
+            if(num % 2 == 0) {
+                statisticsEven.addNumber(num);
+            }else {
+                statisticsOdd.addNumber(num);
+            }
+            
+        }
+        
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
     }
 }
